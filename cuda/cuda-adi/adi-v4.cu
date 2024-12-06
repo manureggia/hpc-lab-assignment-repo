ff03c05b2ebfc2be9faaea8597d1ea7cfaf26f35
user283512 <283512@studenti.unimore.it>
2024-12-06 17:20:41 +0100

    In memoria unificata solo X,B mentre A viene copiata in GPU

--- cuda/cuda-adi/adi-v4.cu
+++ cuda/cuda-adi/adi-v4.cu
@@ -37,16 +37,16 @@ void print_array(int n, DATA_TYPE *X)
 }
 
 // Confronta due matrici per verificare la correttezza
-int compare_matrices(DATA_TYPE *X_host, DATA_TYPE *X_copyice, int n)
+int compare_matrices(DATA_TYPE *X, DATA_TYPE *X_copy, int n)
 {
   int return_value = 1;
   for (int i = 0; i < n; i++)
   {
     for (int j = 0; j < n; j++)
     {
-      if (fabs(X_host[i * n + j] - X_copyice[i * n + j]) > 1e-6)
+      if (fabs(X[i * n + j] - X_copy[i * n + j]) > 1e-6)
       {
-        printf("Mismatch at (%d, %d): Host = %f, Device = %f\n", i, j, X_host[i * n + j], X_copyice[i * n + j]);
+        printf("Mismatch at (%d, %d): Host = %f, Device = %f\n", i, j, X[i * n + j], X_copy[i * n + j]);
         return_value = 0;
       }
     }
@@ -54,78 +54,106 @@ int compare_matrices(DATA_TYPE *X_host, DATA_TYPE *X_copyice, int n)
   return return_value;
 }
 
-// Kernel host
-void kernel_adi_host(int tsteps, int n, DATA_TYPE *X, const DATA_TYPE *A, DATA_TYPE *B)
+void host_adi_col_forward_elimination(int n, DATA_TYPE *X, const DATA_TYPE *A, DATA_TYPE *B)
 {
-	/**
-	 * Questo codice implementa una risoluzione dell'algoritmo Alternating-Direction Implicit (ADI) 
-	 * su una griglia bidimensionale. È strutturato per aggiornare le soluzioni delle equazioni 
-	 * differenziali in due passaggi: uno lungo le colonne e uno lungo le righe della griglia
-	 */
-
-	/** Ad ogni iterazione, il sistema viene aggiornato lungo le colonne e le righe */
+	for (int row = 0; row < n; row++)
+	{
+		for (int col = 1; col < n; col++)
+		{
+			int idx = row * n + col;
+			int prev_idx = row * n + (col - 1);
+			if(B[prev_idx] != 0.f)
+			{
+				X[idx] -= X[prev_idx] * A[idx] / B[prev_idx];
+				B[idx] -= A[idx] * A[idx] / B[prev_idx];
+			}
+		}
+	}
+}
+void host_adi_col_norm(int n, DATA_TYPE *X, const DATA_TYPE *B)
+{
+	for (int col = 0; col < n; col++)
+	{
+		int idx = col * n + (n - 1);
+		if(B[idx] != 0.f)
+			X[idx] /= B[idx];
+	}
+}
+void host_adi_col_back_sostitution(int n, DATA_TYPE *X, const DATA_TYPE *A, const DATA_TYPE *B)
+{
+	for (int row = 0; row < n; row++)
+	{
+		for (int col = n - 2; col >= 0; col--)
+		{
+			int idx = row * n + col;
+			int next_idx = row * n + (col + 1);
+			if (B[idx] != 0.f)
+				X[idx] = (X[idx] - X[next_idx] * A[next_idx]) / B[idx];
+		}
+	}
+}
+void host_adi_row_forward_elimination(int n, DATA_TYPE *X, const DATA_TYPE *A, DATA_TYPE *B)
+{
+	for (int row = 1; row < n; row++)
+	{
+		for (int col = 0; col < n; col++)
+		{
+			int idx = row * n + col;
+			int prev_idx = (row - 1) * n + col;
+			if (B[prev_idx] != 0.f)
+			{
+				X[idx] -= X[prev_idx] * A[idx] / B[prev_idx];
+				B[idx] -= A[idx] * A[idx] / B[prev_idx];
+			}
+		}
+	}
+}
+void host_adi_row_norm(int n, DATA_TYPE *X, const DATA_TYPE *B)
+{
+	for (int col = 0; col < n; col++)
+	{
+		int idx = (n - 1) * n + col;
+		if(B[idx] != 0.f)
+			X[idx] /= B[idx];
+	}
+}
+void host_adi_row_back_sostitution(int n, DATA_TYPE *X, const DATA_TYPE *A, DATA_TYPE *B)
+{
+	for (int row = n - 2; row >= 0; row--) 
+	{
+		for (int col = 0; col < n; col++)
+		{
+			int idx = row * n + col;
+			int next_idx = (row + 1) * n + col;
+			if(B[idx] != 0.f)
+				X[idx] = (X[idx] - X[next_idx] * A[next_idx]) / B[idx];
+		}
+	}
+}
+void host_adi(int tsteps, int n, DATA_TYPE *X, const DATA_TYPE *A, DATA_TYPE *B)
+{
+	/** At each iteration, the system is updated along the columns and rows */
 	for (int t = 0; t < tsteps; t++)
 	{
-		/**
-		 * Aggiornamento lungo le colonne:
-		 * Per ogni riga:
-		 * 1. si applica l'eliminazione in avanti per ridurre il sistema tridiagonale lungo le colonne.
-		 * 2. X rappresenta il vettore delle soluzioni.
-		 * 3. A e B rappresentano i coefficienti delle equazioni differenziali.
-		 */
-		for (int row = 0; row < n; row++)
-		{
-			for (int col = 1; col < n; col++)
-			{
-				X[row * n + col] -= X[row * n + col - 1] * A[row * n + col] / B[row * n + col - 1];
-				B[row * n + col] -= A[row * n + col] * A[row * n + col] / B[row * n + col - 1];
-			}
-		}
+		// -----------------------------------
+		// Update along the columns
+		// -----------------------------------
+		// Forward elimination is performed
+		host_adi_col_forward_elimination(n, X, A, B);
+		// Normalization
+		host_adi_col_norm(n, X, B);
+		// Back Substitution is performed
+		host_adi_col_back_sostitution(n, X, A, B);
 
-		/**
-		 * Normalizzazione:
-		 * Il valore della soluzione nella parte inferiore della colonna è normalizzato dividendo 
-		 * per il coefficiente B
-		 */
-		for (int col = 0; col < n; col++)
-			X[col * n + (n - 1)] /= B[col * n + (n - 1)];
-
-		/**
-		 * Sostituzione all'indietro (Back Substitution):
-		 * Dopo l'eliminazione in avanti, si risolvono i valori risalendo lungo la colonna
-		 */
-		for (int row = 0; row < n; row++)
-			for (int col = 0; col < n - 2; col++)
-				X[row * n + (n - col - 2)] = (X[row * n + (n - col - 2)] - X[row * n + (n - col - 3)] * A[row * n + (n - col - 3)]) / B[row * n + (n - col - 3)];
-		
-		/**
-		 * Aggiornamento lungo le righe:
-		 * Qui il processo si applica lungo le righe.
-		 * Stesso approccio dell'eliminazione lungo le colonne, ma con iterazione spaziale lungo i1.
-		 */
-		for (int row = 1; row < n; row++)
-		{
-			for (int col = 0; col < n; col++)
-			{
-				X[row * n + col] -= X[(row - 1) * n + col] * A[row * n + col] / B[(row - 1) * n + col];
-				B[row * n + col] -= A[row * n + col] * A[row * n + col] / B[(row - 1) * n + col];
-			}
-		}
-
-		/**
-		 * Normalizzazione:
-		 * Si normalizza l'ultima riga dividendo per B.
-		 */
-		for (int col = 0; col < n; col++)
-			X[(n - 1) * n + col] /= B[(n - 1) * n + col];
-
-		/**
-		 * Back-substitution:
-		 * Anche in questo caso, si risolve il sistema risalendo lungo le righe
-		 */
-		for (int row = 0; row < n - 2; row++)
-			for (int col = 0; col < n; col++)
-				X[(n - 2 - row) * n + col] = (X[(n - 2 - row) * n + col] - X[(n - row - 3) * n + col] * A[(n - 3 - row) * n + col]) / B[(n - 2 - row) * n + col];
+		// -----------------------------------
+		// Update along the rows
+		// -----------------------------------
+		// Forward elimination is performed
+		host_adi_row_forward_elimination(n, X, A, B);
+		// Normalization
+		host_adi_row_norm(n, X, B);
+		// Back Substitution is performed
+		host_adi_row_back_sostitution(n, X, A, B);
 	}
 }
 
@@ -306,20 +334,22 @@ int main()
 	const int bytes = sizeof(DATA_TYPE) * n * n;
 	struct timespec rt[2];
 
-	// Lato GPU sono necessari i seguenti dati:
-	// - X[] lettura/scrittura
-	// - B[] lettura/scrittura
-	// - A[] solo lettura
-	// Quindi possiamo usare la memoria unificata per queste 3 variabili
+	// The following data are needed on the GPU side:
+	// - X[] read/write
+	// - B[] read/write
+	// - A[] read only
+	// So we can use the unified memory for these 2 variables
 	// X=d_X
 	// B=d_B
-	// A=d_A
+	// While A can be copyied to GPU with cudaMalloc
 
-	DATA_TYPE *X, *A, *B;
-	gpuErrchk(cudaMallocManaged(&A, bytes));
+	DATA_TYPE *X, *B;
 	gpuErrchk(cudaMallocManaged(&X, bytes));
 	gpuErrchk(cudaMallocManaged(&B, bytes));
+	DATA_TYPE *A_dev;
+	gpuErrchk(cudaMalloc(&A_dev, bytes));
 
+	DATA_TYPE* A_host = (DATA_TYPE*)malloc(bytes);
 	DATA_TYPE* X_copy = (DATA_TYPE*)malloc(bytes);
 	DATA_TYPE* B_copy = (DATA_TYPE*)malloc(bytes);
 
@@ -330,17 +360,18 @@ int main()
 		{
 			int idx 			= i * n + j;
 			X[idx] 				= ((DATA_TYPE)i * (j + 1) + 1) / n;
-			A[idx] 				= ((DATA_TYPE)i * (j + 2) + 2) / n;
 			B[idx] 				= ((DATA_TYPE)i * (j + 3) + 3) / n;
+			A_host[idx] 	= ((DATA_TYPE)i * (j + 2) + 2) / n;
 			X_copy[idx] 	= ((DATA_TYPE)i * (j + 1) + 1) / n;
 			B_copy[idx] 	= ((DATA_TYPE)i * (j + 3) + 3) / n;
 		}
 	}
+	gpuErrchk(cudaMemcpy(A_dev, A_host, bytes, cudaMemcpyHostToDevice));
 
 	// call ADI on host
 	{
 		clock_gettime(CLOCK_REALTIME, rt);
-		kernel_adi_host(tsteps, n, X_copy, A, B_copy);
+		host_adi(tsteps, n, X_copy, A_host, B_copy);
 		clock_gettime(CLOCK_REALTIME, rt + 1);
 
 		double wt = (rt[1].tv_sec - rt[0].tv_sec) + 1.0e-9 * (rt[1].tv_nsec - rt[0].tv_nsec);
@@ -356,70 +387,63 @@ int main()
 
 		clock_gettime(CLOCK_REALTIME, rt);
 		/**
-		 * Come abbiamo visto nell'implementazione lato host, l'algoritmo adi si compone di diverse
-		 * passi fondamentali:
-		 * [1] Aggiornamento lungo le colonne: 
-		 * 		[1.1] eliminazione in avanti (Forward Elimination)
-		 * 		[1.2] normalizzazione
-		 * 		[1.3] sostituzione all'indietro (Back Substitution)
-		 * [2] Aggiornamento lungo le righe:
-		 * 		[2.1] eliminazione in avanti
-		 * 		[2.2] normalizzazione
-		 * 		[2.3] sostituzione all'indietro
-		 * 
-		 * Nell'algoritmo ADI, alcune operazioni possono essere eseguite in parallelo perché non dipendono 
-		 * direttamente dai risultati degli altri calcoli per ogni iterazione spaziale.
-		 * Le operazioni possono essere parallelizzate per righe durante l'aggiornamento lungo le colonne e 
-		 * per colonne durante l'aggiornamento lungo le righe.
+		 * As we saw in the host-side implementation, the adi algorithm consists of several
+		 * basic steps:
+		 * [1] Updating along columns: 
+		 * 		[1.1] forward elimination (Forward Elimination).
+		 * 		[1.2] normalization
+		 * 		[1.3] backward substitution (Back Substitution).
+		 * [2] Update along rows:
+		 * 		[2.1] forward elimination
+		 * 		[2.2] normalization
+		 * 		[2.3] back substitution
+		 * 		 * 
+		 * In the ADI algorithm, some operations can be performed in parallel because they do not depend 
+		 * directly on the results of other computations for each spatial iteration.
+		 * Operations can be parallelized for rows during the update along columns and 
+		 * by columns when updating along rows.
 		 */
 		for (int t = 0; t < tsteps; t++)
 		{
 			// ------------------------------------------------
-			// [1] Aggiornamento lungo le colonne
+			// Update along the columns
 			// ------------------------------------------------
-			// [1.1] eliminazione in avanti (Forward Elimination): 
-			// gli aggiornamenti lungo una colonna di una stessa riga dipendono dal valore precedente
-			// nella stessa riga, quindi non è parallelizzabile lungo le colonne,
-			// ma l'operazione per righe differenti è indipendente.
+			// Forward Elimination: updates along a column of the same row depend on the previous value
+			// in the same row, so it is not parallelizable along columns,
+			// but the operation for different rows is independent.
 			blockLayout = dim3(1, BLOCK_SIZE, 1);
 			gridLayout 	= dim3(1, nBlocks, 1);
-			kernel_column_forward_elimination<<<gridLayout, blockLayout>>>(n, X, A, B);
+			kernel_column_forward_elimination<<<gridLayout, blockLayout>>>(n, X, A_dev, B);
 			gpuErrchk(cudaPeekAtLastError());
 			gpuErrchk(cudaDeviceSynchronize());
-			// [1.2] normalizzazione: 
-			// parallelizzabile per riga; ogni riga è indipendente.
+			// Normalization: parallelizable per row; each row is independent.
 			kernel_column_norm<<<gridLayout, blockLayout>>>(n, X, B);
 			gpuErrchk(cudaPeekAtLastError());  
 			gpuErrchk(cudaDeviceSynchronize());
-			// [1.3] sostituzione all'indietro (Back Substitution):
-			// parallelizzabile per riga; anche qui, ogni riga rappresenta un sistema tridiagonale indipendente.
-			// L'operazione lungo colonne dipende dai valori precedenti della stessa riga.
-			kernel_column_back_sostitution<<<gridLayout, blockLayout>>>(n, X, A, B);
+			// Back Substitution: parallelizable per row; again, each row represents an independent tridiagonal system.
+			// The operation along columns depends on the previous values of the same row.
+			kernel_column_back_sostitution<<<gridLayout, blockLayout>>>(n, X, A_dev, B);
 			gpuErrchk(cudaPeekAtLastError());  
 			gpuErrchk(cudaDeviceSynchronize());
 
 			// ------------------------------------------------
-			// [2] Aggiornamento lungo le righe
+			// Update along the columns
 			// ------------------------------------------------
-			// [2.1] eliminazione in avanti:
-			// parallelizzabile per colonna; ogni colonna della griglia rappresenta un sistema 
-			// tridiagonale indipendente.
-			// Gli aggiornamenti lungo una riga dipendono dal valore precedente nella stessa colonna, 
-			// quindi non è parallelizzabile lungo le righe, ma può essere parallelo tra colonne diverse.
+			// Forward Elimination: parallelizable by column; each column of the grid represents an independent 
+			// independent tridiagonal. Updates along a row depend on the previous value in the same column, 
+			// so it is not parallelizable along rows, but can be parallel between different columns.
 			blockLayout = dim3(BLOCK_SIZE, 1, 1);
 			gridLayout 	= dim3(nBlocks, 1, 1);
-			kernel_row_forward_elimination<<<gridLayout, blockLayout>>>(n, X, A, B);
+			kernel_row_forward_elimination<<<gridLayout, blockLayout>>>(n, X, A_dev, B);
 			gpuErrchk(cudaPeekAtLastError());  
 			gpuErrchk(cudaDeviceSynchronize());
-			// [2.2] normalizzazione: 
-			// parallelizzabile per colonna; ogni colonna è indipendente.
+			// Normalization: parallelizable by column; each column is independent.
 			kernel_row_norm<<<gridLayout, blockLayout>>>(n, X, B);
 			gpuErrchk(cudaPeekAtLastError());  
 			gpuErrchk(cudaDeviceSynchronize());
-			// [2.3] sostituzione all'indietro:
-			// parallelizzabile per colonna; simile all'eliminazione in avanti, 
-			// ogni colonna rappresenta un sistema tridiagonale indipendente.
-			kernel_row_back_sostitution<<<gridLayout, blockLayout>>>(n, X, A, B);
+			// Back Substitution: parallelizable by column; similar to forward elimination, 
+			// each column represents an independent tridiagonal system.
+			kernel_row_back_sostitution<<<gridLayout, blockLayout>>>(n, X, A_dev, B);
 			gpuErrchk(cudaPeekAtLastError());  
 			gpuErrchk(cudaDeviceSynchronize());
 		}    
@@ -431,7 +455,7 @@ int main()
 	}
 
 
-	if (compare_matrices(X, X_copy, n))
+	if (compare_matrices(X, X_copy, n) && compare_matrices(B, B_copy, n))
 	{
 		printf("Risultati Host e Device CORRETTI!\n");
 	}
@@ -444,8 +468,9 @@ int main()
 
 	free(X_copy);
 	free(B_copy);
+	free(A_host);
 	gpuErrchk(cudaFree(X));
-	gpuErrchk(cudaFree(A));
 	gpuErrchk(cudaFree(B));
+	gpuErrchk(cudaFree(A_dev));
 	return 0;
 }
